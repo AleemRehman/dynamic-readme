25848a22ded73745a38fe76d5cc5994710a72f10
Aleem Ur-Rehman <contact@aleemrehman.co>
2022-03-18 15:45:08 +0000

    some more handling around files

--- file_handler.py
+++ file_handler.py
@@ -1,14 +1,22 @@
+import os
+import pathlib
 
+root = pathlib.Path(__file__).parent.resolve()
 class File:
   def __init__(self):
     return
   
-  def template_handler(self, template_path):
+  def template_handler(self, template_name):
     # check if the template exists
     #   - duplicate the contents of the file if it does exist
     #   - return a success flag and the contents of the template
-    if os.path.exists(template_path):
-      with open(template_path,"r") as template_file:
+    path_name = root
+
+    if type(path_name) == str:
+      path_name = root + path_name
+
+    if os.path.exists(path_name):
+      with open(path_name,"r") as template_file:
         template = template_file.read()
         if '[[INSERT]]' in template:
           print("there's a dynamic placeholder")
